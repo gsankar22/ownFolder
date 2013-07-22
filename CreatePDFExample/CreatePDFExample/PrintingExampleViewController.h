88f5c3affad4236678181fb7c731e982a50dd930
gowriq4 <gowriq4@gmail.com>
2013-07-22 16:29:05 +0530

    Revert "framework add"
    This reverts commit 986d618ea97b6fc5759a4f0b4f2cb3d14839db27.

--- CreatePDFExample/CreatePDFExample/PrintingExampleViewController.h
+++ CreatePDFExample/CreatePDFExample/PrintingExampleViewController.h
@@ -18,13 +18,10 @@
 @interface PrintingExampleViewController : UIViewController <MFMailComposeViewControllerDelegate, UIActionSheetDelegate, QLPreviewControllerDataSource>{
     
     NSString* pdfFilePath;
-    
-    UIImageView*img;
 }
 
 - (IBAction)pdfPressed:(id)sender;
 
 @property (nonatomic, retain) NSString* pdfFilePath;
-
 
 @end
